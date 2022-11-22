cb09d934bc7c12ef5fdc6d22a57a20cdcfbd5e65
glenhayoge <glenhayoge@gmail.com>
2022-11-23 08:05:18 +1000

    article cards, dynamic routes (#5)

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const { withContentlayer } = require('next-contentlayer');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'media-exp1.licdn.com'],
+    dangerouslyAllowSVG: true,
+  }
 }
+
 
 module.exports = withContentlayer(nextConfig)
---
+++ pages/article/[slug].js
@@ -0,0 +1,40 @@
+import { allArticles } from 'contentlayer/generated';
+import { NextSeo } from 'next-seo';
+import { SingleArticle } from '../../components/SingleArticle';
+
+
+const SinglePost = ({ article }) => {
+  console.log(article);
+
+  return (
+    <>
+      <NextSeo title={article.title} description={article.seoDescription} />
+
+      <SingleArticle
+        image={article.image}
+        title={article.title}
+        category={article.category}
+        author={article.author}
+      >
+        
+      </SingleArticle>
+    </>
+  );
+};
+
+export default SinglePost;
+
+export async function getStaticPaths() {
+  return {
+    paths: allArticles.map((article) => ({
+      params: { slug: article.slug },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const article = allArticles.find((article) => article.slug === params.slug);
+
+  return { props: { article } };
+}
