28af3daedd92c8788686bdd0b64ad8e89361d108
Glen Hayoge <glenhayoge@gmail.com>
2023-02-20 15:32:49 +1000

    removed view counter from book details

--- components/BookDetails.jsx
+++ components/BookDetails.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import ViewCounter from "./ViewCounter";
+// import ViewCounter from "./ViewCounter";
 
 export const BookDetails = ({
 
