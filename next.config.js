e8f7dfabadf1d035c7ec73548e5e1d8331d77881
Glen Hayoge <glenhayoge@gmail.com>
2023-02-27 20:48:08 +1000

    removed swcMinify: false

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const { withContentlayer } = require('next-contentlayer');
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
   images: {
     domains: ['images.unsplash.com', 'i.pravatar.cc','previews.123rf.com',],
     dangerouslyAllowSVG: true,
