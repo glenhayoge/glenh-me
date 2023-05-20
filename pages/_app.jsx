31f8323fec44d2374ba5af7a47d8989ad7b737a1
Glen Hayoge <glenhayoge@gmail.com>
2023-05-21 01:37:57 +1000

    updated meta image to read from github url

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -29,22 +29,22 @@ export default function App({ Component, pageProps }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <title>My digital commonplace on the internet  for logic based art & creativity</title>
+        <title>Glen's digital commonplace for logic based art & creativity</title>
         <meta name="viewport" content="width=device-width,   initial-scale=1"/>
-        <meta name="title" content="A digital commonplace for logic based arts & creativity"/>
+        <meta name="title" content="Glen's digital commonplace for logic based arts & creativity"/>
         <meta name="description" content="TL;DR : A digest of a perpetual work in progress"/>
         <meta property="og:type" content="website"/>
         <meta property="og:image:type" content="image/png" />
         <meta property="og:url" content="https://www.glenh.me/"/>
-        <meta property="og:title" content="A digital commonplace for logic based arts & creativity"/>
+        <meta property="og:title" content="Glen's digital commonplace for logic based arts & creativity"/>
         <meta property="og:description" content="TL;DR : A digest of a perpetual work in progress"/>
-        <meta property="og:image" content="https://www.glenh.me/assets/ggh-meta-image.png"/>
+        <meta property="og:image" content="https://github.com/glenhayoge/glenh-me/blob/main/assets/ggh-meta-image.png"/>
 
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://www.glenh.me/"/>
-        <meta property="twitter:title" content="My digital commonplace for logic based arts & creativity"/>
+        <meta property="twitter:title" content="Glen's digital commonplace for logic based arts & creativity"/>
         <meta property="twitter:description" content="TL;DR : A digest of a perpetual work in progress"/>
-        <meta property="twitter:image" content="https://www.glenh.me/assets/ggh-meta-image.png"/>
+        <meta property="twitter:image" content="https://github.com/glenhayoge/glenh-me/blob/main/assets/ggh-meta-image.png"/>
       </Head>
       <ThemeProvider enableSystem={true} attribute="class">
         <Navbar />
