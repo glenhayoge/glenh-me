295a918e9ef9e4d44f8de10f18032f0d0504e941
Glen Hayoge <glenhayoge@gmail.com>
2023-11-01 06:28:49 +1000

    added w and h

--- components/Photography.jsx
+++ components/Photography.jsx
@@ -21,36 +21,36 @@ export default function Photography() {
     <Image
           src="https://images.unsplash.com/photo-1698700654311-8b8b2b05e227?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNTUxNg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" // Replace with your actual external image URL
           alt="Projects Image"
-          width="400px"
-          height="400px"
+          width="400"
+          height="400"
           className="rounded-t-lg"
         />
         </a>
-        {/* <div className=" pt-3 ">
-          <h5 className="text-gray-500 text-lg font-medium ">
-            Card title
+        <div className=" p-3 ">
+          <h5 className="text-gray-500 text-sm font-medium ">
+            A Bulb Onion Plot, WHP
           </h5>
-        </div> */}
+        </div>
       </div>
     </div>
   </div>
   <div className="w-full md:w-1/2 lg:w-1/4 p-4">
     <div className="flex justify-center">
     <div className="rounded max-w-sm bg-gray-300/25 dark:bg-gray-700/25 outline-gray-200 dark:outline-gray-600">
-    <a href="https://images.unsplash.com/photo-1698700654103-99b994a34c2a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNTQ1MA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" target="_blank" rel="noreferrer">
+    <a href="https://images.unsplash.com/photo-1698700654109-825adf37f6c6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNDUwNw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" target="_blank" rel="noreferrer">
     <Image
-          src="https://images.unsplash.com/photo-1698700654103-99b994a34c2a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNTQ1MA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" // Replace with your actual external image URL
+          src="https://images.unsplash.com/photo-1698700654109-825adf37f6c6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNDUwNw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" // Replace with your actual external image URL
           alt="Projects Image2"
-          width="400px"
-          height="400px"
+          width="400"
+          height="400"
           className="rounded-t-lg"
         />
         </a>
-        {/* <div className=" pt-3 ">
-          <h5 className="text-gray-500 text-lg font-medium ">
-            Card title
+        <div className=" p-3 ">
+          <h5 className="text-gray-500 text-sm font-medium ">
+            An Array of Tomatoes, EHP
           </h5>
-        </div> */}
+        </div>
       </div>
     </div>
   </div>
@@ -61,16 +61,16 @@ export default function Photography() {
         <Image
           src="https://images.unsplash.com/photo-1698700654105-d4e75360dc99?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNTM3OQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" // Replace with your actual external image URL
           alt="Projects Image3"
-          width="400px"
-          height="400px"
+          width="400"
+          height="400"
           className="rounded-t-lg"
         />
         </a>
-        {/* <div className=" pt-3 ">
-          <h5 className="text-gray-500 text-lg font-medium ">
-            Card title
+        <div className=" p-3 ">
+          <h5 className="text-gray-500 text-sm font-medium ">
+          A Child Walking Home, EHP
           </h5>
-        </div> */}
+        </div>
       </div>
     </div>
   </div>
@@ -81,17 +81,17 @@ export default function Photography() {
         <Image
           src="https://images.unsplash.com/photo-1698700654176-c42201d7ba49?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5ODcyNTEyMw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" // Replace with your actual external image URL
           alt="Projects Image4"
-          width="400px"
-          height="400px"
+          width="400"
+          height="400"
           className="rounded-t-lg"
         />
 
         </a>
-        {/* <div className=" pt-3 ">
-          <h5 className="text-gray-500 text-lg font-medium ">
-            Card title
+        <div className=" p-3 ">
+          <h5 className="text-gray-500 text-sm font-medium ">
+           A Village Hut, WHP
           </h5>
-        </div> */}
+        </div>
       </div>
     </div>
   </div>
