0dddb62a14b42587d50e26574268b10b6ae0aeb3
Glen Hayoge <glenhayoge@gmail.com>
2024-01-21 13:46:11 +1000

    added merlin mis

--- components/WebApps.jsx
+++ components/WebApps.jsx
@@ -547,6 +547,58 @@ export default function WebApps() {
       </div>
     </div>
   </div>
+  <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+    <div className="block rounded-lg outline outline-1 bg-gray-300/25 dark:bg-gray-700/25 outline-gray-200 dark:outline-gray-600 max-w-sm text-center">
+      <div className="py-3 px-6 font-bold border-b text-left border-gray-300 dark:border-gray-500 text-gray-500">
+        <div className="text-center">
+          Merlin-Management Information System
+        </div>
+      </div>
+      <div className="p-6 items-left flex-left">
+        <p className="text-gray-500 text-center text-sm tracking-tight leading-tight mb-4">
+          Merlin-Management Information System - https://merlinmis.org
+        </p>
+        <Link href="https://merlinmis.org/" target="_blank">
+          <button className="bg-gray-500/25 hover:scale-[1.02]  text-gray-500 dark:text-gray-400 text-xs py-1 px-2 rounded-xl inline-flex items-center">
+            <span>Live site</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 ml-1 dark:text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          </button>
+        </Link>
+      </div>
+      <div className="py-3 text-xl px-6 border-t border-gray-300 dark:border-gray-500 text-gray-400">
+      <span>
+          <i className="devicon-django-plain p-1"></i>
+        </span>
+        <span>
+          <i className="devicon-docker-plain p-1"></i>
+        </span>
+        <span>
+          <i className="devicon-python-plain p-1"></i>
+        </span>
+        <span>
+          <i className="devicon-javascript-plain p-1"></i>
+        </span>
+         <span>
+         <i className="devicon-tailwindcss-plain p-1"></i>
+        </span>
+        <span><i className="devicon-github-original p-1"></i>
+        </span>
+      </div>
+    </div>
+  </div>
 </div>
 </div>
 
