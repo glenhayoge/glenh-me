b68d933ebe932c56605cb33c6dd1585e87ee2af9
Glen Hayoge <glenhayoge@gmail.com>
2024-02-27 09:41:18 +1000

    added notice in subscription form

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -69,21 +69,22 @@ export default function Subscribe() {
       {openModal &&
         <div className='fixed z-100 top-0 left-0 w-full dark:bg-black bg-white h-full flex justify-center items-center'>
           <div className='max-w-[480px] border border-gray-600 bg-gray-100 dark:bg-black px-4 shadow-lg py-4 rounded-md'>
-            <h2 className='text-md uppercase font-medium  dark:text-gray-400 text-gray-900 text-center py-4 px-4 mb-4'>Subscribe to My Newsletter</h2>
+            <h2 className='text-md uppercase font-medium  dark:text-gray-400 text-gray-900 text-center  px-4 '>Subscribe to My Newsletter</h2>
+            <p class="text-xs mb-4 text-gray-500 dark:text-gray-600 text-center">I pinky promise not to spam or publish your email addresses.</p>
             <div>
   <label for="price" class="block text-sm font-medium leading-6 dark:text-gray-400 text-gray-900">Email Address</label>
   <div class="relative mt-2 rounded-md shadow-sm">
     <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
       <span class="text-gray-500 sm:text-sm">@</span>
     </div>
-    <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email Address"/>
+    <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Type Your Email Address"/>
   </div>
 </div>
             <div className='flex justify-between items-center pt-4'>
               {/* <div className='text-sm font-medium text-gray-700'>Newsletter</div> */}
               <button
                 type='button'
-                className='h-8 px-2 text-sm rounded-md bg-gray-700 dark:bg-yellow-400 text-white dark:text-black'
+                className='h-8 px-2 text-xs tracking-wider uppercase rounded-md bg-gray-700 dark:bg-yellow-400 text-white dark:text-black'
                 onClick={handleModal}
               >
                 Subscribe
