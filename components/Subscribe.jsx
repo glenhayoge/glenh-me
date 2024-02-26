c0e3bfb818769683af39468883192af2201d01c9
Glen Hayoge <glenhayoge@gmail.com>
2024-02-27 09:08:16 +1000

    added username snippet

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -67,12 +67,26 @@ export default function Subscribe() {
             </div>
       </div>
       {openModal &&
-        <div className='fixed top-0 left-0 w-full h-full bg-gray-300 flex justify-center items-center'>
-          <div className='max-w-[460px] bg-white shadow-lg py-2 rounded-md'>
-            <h2 className='text-sm font-medium text-gray-900 border-b border-gray-300 py-3 px-4 mb-4'>Subscribe to Newsletter</h2>
-            <div className='px-4 pb-4'>
-              <p className='text-sm font-medium text-gray-700'>Insert newsletter subscription form here</p>
-            </div>
+        <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center'>
+          <div className='max-w-[460px] bg-gray-100 dark:bg-gray-600 px-2 shadow-lg py-2 rounded-md'>
+            <h2 className='text-sm font-medium text-gray-900  py-3 px-4 mb-4'>Subscribe to Newsletter</h2>
+            <div>
+  <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
+  <div class="relative mt-2 rounded-md shadow-sm">
+    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+      <span class="text-gray-500 sm:text-sm">$</span>
+    </div>
+    <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"/>
+    <div class="absolute inset-y-0 right-0 flex items-center">
+      <label for="currency" class="sr-only">Currency</label>
+      <select id="currency" name="currency" class="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+        <option>USD</option>
+        <option>CAD</option>
+        <option>EUR</option>
+      </select>
+    </div>
+  </div>
+</div>
             <div className='border-t border-gray-300 flex justify-between items-center px-4 pt-2'>
               <div className='text-sm font-medium text-gray-700'>Newsletter</div>
               <button
