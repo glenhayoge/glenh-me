f3b2829aabb57ba1c347ede006b0226c39d6bd00
Glen Hayoge <glenhayoge@gmail.com>
2024-02-27 16:12:22 +1000

    added close button to subscribe modal

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -68,7 +68,17 @@ export default function Subscribe() {
       </div>
       {openModal &&
         <div className='fixed z-100 top-0 left-0 w-full dark:bg-black bg-white h-full flex justify-center items-center'>
+          
           <div className='max-w-[480px] border border-gray-600 bg-gray-100 dark:bg-black px-4 shadow-lg py-4 rounded-md'>
+          <div className='flex justify-between items-right'>
+              <button
+                type='button'
+                className='h-6 px-1 uppercase rounded-full items-right text-right bg-gray-700 dark:bg-yellow-400 text-white dark:text-black'
+                onClick={handleModal}
+              >
+               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x w-4 h-4" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+              </button>
+            </div>
             <h2 className='text-md uppercase font-medium  dark:text-gray-400 text-gray-900 text-center  px-4 '>Subscribe to My Newsletter</h2>
             <p class="text-xs mb-4 text-gray-500 dark:text-gray-600 text-center">I pinky promise not to spam or publish your email addresses.</p>
             <div>
