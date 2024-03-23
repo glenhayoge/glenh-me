2a336af0521d543c2ed1726486ec3dce3290ee14
Glen Hayoge <glenhayoge@gmail.com>
2024-03-24 09:02:19 +1000

    updated article image

--- components/TimeAgo.jsx
+++ components/TimeAgo.jsx
@@ -4,7 +4,6 @@ import TimeAgo from 'react-timeago'
 import frenchStrings from 'react-timeago/lib/language-strings/fr'
 import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
 
-
 import { useEffect } from "react";
 export default function TimeAgoComponent() {
  
