09da0d2eeb3e0fa10d88502512db889ae08f7cf7
Glen Hayoge <glenhayoge@gmail.com>
2024-05-26 10:36:23 +1000

    updated ui styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
         />
       </div> */}
       <section className="bg-white dark:bg-black w-full ">
-    <div class="grid container px-2 w-full lg:w-8/12 lg:pl-20 md:px-8  mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div class="grid container py-8 px-2 w-full lg:w-8/12 lg:pl-20 md:px-8  mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className=" place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">developer. designer.  creator.</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">a digest of a perpetual work in progress</p>
@@ -45,7 +45,7 @@ const Header = () => {
                 Know About Me
             </a> 
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex flex-col items-center">
+        <div className="lg:mt-0 lg:col-span-5 lg:flex flex-col items-center">
 
             <Image 
                   width={360}
