8e6b9e6140ec4059fdbd745c00f26a8879c6b14d
Glen Hayoge <glenhayoge@gmail.com>
2024-05-26 15:30:10 +1000

    added bullet points

--- components/Header.jsx
+++ components/Header.jsx
@@ -32,9 +32,9 @@ const Header = () => {
       </div>
    
       <section className="dark:bg-black w-full ">
-    <div class="grid container  py-24 mb-8 px-2 w-full lg:w-8/12 lg:pl-10 md:px-4 mx-auto lg:gap-8 xl:gap-0 lg:py-6 lg:grid-cols-12">
-        <div className=" z-20 place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight text-gray-600 leading-none md:text-5xl xl:text-6xl dark:text-white"><span class="text-yellow-400">d</span>eveloper. <span class="text-yellow-400">d</span>esigner.  <span class="text-yellow-400">c</span>reator.</h1>
+    <div class="grid px-2 w-full md:px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
+        <div className=" z-20 place-self-center lg:pl-36 lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight text-gray-600 leading-none md:text-5xl xl:text-6xl dark:text-white"><span class="text-yellow-400">d</span>eveloper &#x2022; <span class="text-yellow-400">d</span>esigner &#x2022; <span class="text-yellow-400">c</span>reator &#x2022;</h1>
             <p className="max-w-2xl tracking-loose mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">a digest of a perpetual work in progress</p>
             <div className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center dark:text-white text-gray-500 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Hire My Service
