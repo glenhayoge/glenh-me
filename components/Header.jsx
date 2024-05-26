dff749c1ba043c713905c9bbe3584b47672858a1
Glen Hayoge <glenhayoge@gmail.com>
2024-05-26 10:19:56 +1000

    updated home hero component

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import tribalPattern from '../assets/tribal-pattern.jpg';
 import Head from "next/head";
 import Link from 'next/link'
 import { motion } from "framer-motion";
+import propic from "../assets/glenh-photo.png";
 
 const { heroContent, heroWrapper, imageWrapper } = styles;
 const subTitle = { 
@@ -11,6 +12,14 @@ const subTitle = {
   fontSize: '30px',
   textAlign: 'center',
  }; 
+ const signature = { 
+  fontFamily: 'Mrs Saint Delafield, cursive',
+  fontSize: '40px',
+  textAlign: 'center',
+ };
+ const heroImage = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+}
 const Header = () => {
   return (
     <div className={heroWrapper}>
@@ -20,11 +29,39 @@ const Header = () => {
           src={tribalPattern}
           object-position="center"
           alt="background hero"
-          className="opacity-10 absolute bg-blend-color opacity-50 top-0 left-0 w-full lg:px-48 md:px-0 h-80 object-cover"
+          className="opacity-10 absolute bg-blend-color opacity-50 top-0 left-0 w-full lg:px-12 md:px-0 h-80 object-cover"
         />
       </div> */}
+      <section className="bg-white dark:bg-black w-full ">
+    <div class="grid max-w-5xl px-2 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">developer. designer.  creator.</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A Digest of a Perpetual Work in Progress</p>
+            <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Hire My Service
+                <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <a href="/about" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Know About Me
+            </a> 
+        </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex flex-col items-center">
 
-      <div className={heroContent}>
+            <Image 
+                  width={360}
+                  height={240}
+                  src={propic}
+            
+            />
+            {/* <p className="mt-2 text-center">Your caption here</p> */}
+            <h5 style={signature} className="mt-4 text-gray-600 dark:text-yellow-400 text-center text-xl mb-4">
+              GlenGHayoge
+            </h5>
+        </div>                
+    </div>
+</section>
+
+      {/* <div className={heroContent}>
       <div className="flex justify-center md:mt-15 space-y-6 my-8 items-center">
       <Link href="#" className="text-gray-400 hover:text-gray-500 ">
             <span className="sr-only">GlenGH Logo</span>
@@ -82,7 +119,7 @@ const Header = () => {
         by a developer, designer & creator.
         </p>
         </div>
-      </div>
+      </div> */}
     </div>
     
   );
