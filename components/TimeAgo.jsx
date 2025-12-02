0b5f08045fa0a41250879cd890f65412c17f59a1
Glen Hayoge <glenhayoge@gmail.com>
2025-12-03 05:37:30 +1000

    added day 4 article, web design fundamentals

--- components/TimeAgo.jsx
+++ components/TimeAgo.jsx
@@ -10,7 +10,7 @@ export default function TimeAgoComponent() {
   return (
     <>
       <div className=" px-6 text-xs italic text-gray-500">
-      Updated: <TimeAgo date="December 2, 2025 5:50 am" locale="pg"  />
+      Updated: <TimeAgo date="December 3, 2025 5:36 am" locale="pg"  />
       </div>
     </>
   );
